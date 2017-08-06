760cb76c0dbacf9e6ffc47c80525bfd4aa339d8b
nihar <nihartar@gmail.com>
2017-08-06 07:58:21 +0530

    Added queries to DbQueryHelper.

--- app/src/main/java/com/bangalorebuses/DbQueryHelper.java
+++ app/src/main/java/com/bangalorebuses/DbQueryHelper.java
@@ -3,6 +3,8 @@ package com.bangalorebuses;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
+import java.util.ArrayList;
+
 import static com.bangalorebuses.Constants.DIRECTION_UP;
 
 class DbQueryHelper
@@ -37,10 +39,63 @@ class DbQueryHelper
         }
     }
 
+    public static ArrayList<Route> getRoutesWithNumber(SQLiteDatabase db, String routeName)
+    {
+
+    }
+
+    public static ArrayList<BusStop> getStopsOnRoute(SQLiteDatabase db, int routeId)
+    {
+
+    }
+
+    public static ArrayList<Route> getRoutesViaStop(SQLiteDatabase db, int stopId)
+    {
+
+    }
+
+    public static ArrayList<String> getRouteDepartureTimings(SQLiteDatabase db, int routeId)
+    {
+
+    }
+
     public static BusStop getStopDetails(SQLiteDatabase db, int stopId)
     {
         BusStop busStop = new BusStop();
-        db.rawQuery("", null);
-        return busStop;
+        Cursor cursor = db.rawQuery("select Stops.StopId, Stops.StopName, Stops.StopLat, Stops.StopLong," +
+                " Stops.StopDirectionName from Stops where Stops.StopId = " + stopId, null);
+        if (cursor.moveToNext())
+        {
+            busStop.setBusStopId(cursor.getInt(cursor.getColumnIndex(BengaluruBusesContract.BusStops.COLUMN_STOP_ID)));
+            busStop.setBusStopName(cursor.getString(cursor.getColumnIndex(BengaluruBusesContract.BusStops.COLUMN_STOP_NAME)));
+            busStop.setLatitude(cursor.getString(cursor.getColumnIndex(BengaluruBusesContract.BusStops.COLUMN_STOP_LAT)));
+            busStop.setLongitude(cursor.getString(cursor.getColumnIndex(BengaluruBusesContract.BusStops.COLUMN_STOP_LONG)));
+            busStop.setBusStopDirectionName(cursor.getString(cursor.getColumnIndex(BengaluruBusesContract.BusStops.COLUMN_STOP_DIRECTION_NAME)));
+            return busStop;
+        }
+        else
+        {
+            cursor.close();
+            return null;
+        }
+    }
+
+    public static ArrayList<BusStop> getStopsWithName(SQLiteDatabase db, String stopName)
+    {
+        Cursor cursor = db.rawQuery("select Stops.StopId, Stops.StopName, Stops.StopLat, Stops.StopLong," +
+                " Stops.StopDirectionName from Stops where Stops.StopName = '" + stopName + "'", null);
+        ArrayList<BusStop> busStops = new ArrayList<>();
+        while (cursor.moveToNext())
+        {
+            BusStop busStop = new BusStop();
+            busStop.setBusStopId(cursor.getInt(cursor.getColumnIndex(BengaluruBusesContract.BusStops.COLUMN_STOP_ID)));
+            busStop.setBusStopName(cursor.getString(cursor.getColumnIndex(BengaluruBusesContract.BusStops.COLUMN_STOP_NAME)));
+            busStop.setLatitude(cursor.getString(cursor.getColumnIndex(BengaluruBusesContract.BusStops.COLUMN_STOP_LAT)));
+            busStop.setLongitude(cursor.getString(cursor.getColumnIndex(BengaluruBusesContract.BusStops.COLUMN_STOP_LONG)));
+            busStop.setBusStopDirectionName(cursor.getString(cursor.getColumnIndex(BengaluruBusesContract.BusStops.COLUMN_STOP_DIRECTION_NAME)));
+            busStops.add(busStop);
+        }
+        cursor.close();
+        return busStops;
     }
 }
